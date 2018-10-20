6f8186f0c2ebe761f2c1903c76e0aa75560ca98f
ZakharenkoIE <Zie140494@gmail.com>
2018-10-20 14:46:30 +0700

    Очередь сравнение Peek и Dequeue

---
+++ Metanit/C# Common/List 8/Linked_List/Queue/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Queue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<int> numbers = new Queue<int>();
+            numbers.Enqueue(3);
+            numbers.Enqueue(5);
+            numbers.Enqueue(7);
+
+            int element = numbers.Dequeue();
+            Console.WriteLine(element);
+
+            Queue < Person > persons = new Queue<Person>();
+            persons.Enqueue(new Person() { Name="Vasya"} );
+            persons.Enqueue(new Person() { Name = "Petya" });
+            persons.Enqueue(new Person() { Name = "Lexa" });
+
+            Console.WriteLine("Peek");
+            Person p = persons.Peek();
+            Console.WriteLine("Выбранный элемент из очереди "+p.Name);
+            foreach (Person pp in persons)
+            {
+                Console.WriteLine(pp.Name);
+            }
+            Console.WriteLine("Dequeue");
+            Person p1 = persons.Dequeue();
+            Console.WriteLine("Выбранный элемент из очереди " + p1.Name);
+            foreach (Person pp in persons)
+            {
+                Console.WriteLine(pp.Name);
+            }
+            Console.ReadLine();
+        }
+    }
+    class Person
+    {
+        public string Name { get; set; }
+    }
+}

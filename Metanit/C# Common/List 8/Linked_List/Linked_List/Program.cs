a0ed756ea7df12e4739f40cb63c1d37ab35beb14
ZakharenkoIE <Zie140494@gmail.com>
2018-10-20 14:29:10 +0700

    Связанные списки на примерах чисел и объектов

--- Metanit/C# Common/List 8/Linked_List/Linked_List/Program.cs
+++ Metanit/C# Common/List 8/Linked_List/Linked_List/Program.cs
@@ -10,7 +10,27 @@ namespace Linked_List
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello world!");
+            LinkedList<int> numbers = new LinkedList<int>();
+            numbers.AddLast(1);//Add Last Node
+            numbers.AddFirst(2);//Add First Node
+            numbers.AddAfter(numbers.Last, 3);
+            foreach (int i in numbers)
+            {
+                Console.WriteLine(i);
+            }
+
+            LinkedList<Person> persons = new LinkedList<Person>();
+
+            LinkedListNode<Person> tom = persons.AddLast(new Person () {Name = "Tom" });
+            persons.AddLast(new Person() { Name = "Vasya" });
+            persons.AddFirst(new Person() { Name = "Petya" });
+            Console.WriteLine(tom.List.First.Value);
+            Console.WriteLine(tom.List.Last.Value);
+            Console.ReadLine();
         }
+    }
+    class Person
+    {
+        public string Name { get; set; }
     }
 }

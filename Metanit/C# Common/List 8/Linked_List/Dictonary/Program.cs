3e5d36b0bd408ccc89e315e7fbcd8929e0168df7
ZakharenkoIE <Zie140494@gmail.com>
2018-10-21 19:07:23 +0700

    Словарь, перебор по ключам и значениям, добавление и удаление элементов из словаря

---
+++ Metanit/C# Common/List 8/Linked_List/Dictonary/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, string> countries = new Dictionary<int, string>();
+            countries.Add(1, "Russia");
+            countries.Add(3, "Great Britain");
+            countries.Add(2, "USA");
+            countries.Add(4, "China");
+
+            foreach (KeyValuePair<int,string> kvp in countries)
+            {
+                Console.WriteLine(kvp.Key + " - " + kvp.Value);
+            }
+
+            //get
+            string country = countries[4];
+            //set
+            countries[4] = "spain";
+
+            countries.Remove(2);
+
+            Console.WriteLine("Delete Element with key 2");
+            Console.WriteLine("After delete");
+
+            foreach (KeyValuePair<int, string> kvp in countries)
+            {
+                Console.WriteLine(kvp.Key + " - " + kvp.Value);
+            }
+
+            Dictionary<char, Person> persons = new Dictionary<char, Person>();
+            persons.Add('v', new Person { Name="Vasya" });
+            persons.Add('p', new Person { Name = "Petya" });
+            persons.Add('l', new Person { Name = "Lexa" });
+
+            Console.WriteLine("key");
+            foreach (char c in persons.Keys)
+            {
+                Console.WriteLine(c);
+            }
+
+            Console.WriteLine("Values");
+            foreach (Person p in persons.Values)
+            {
+                Console.WriteLine(p.Name);
+            }
+
+
+            Console.ReadLine();
+        }
+        class Person
+        {
+            public string Name { get; set; }
+        }
+    }
+}

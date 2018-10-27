3e43a2d28f84f4f09d14283de4966961d152c005
ZakharenkoIE <Zie140494@gmail.com>
2018-10-27 19:23:11 +0700

    Task stopped by token

---
+++ Tasks/Tasks/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Tasks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var cts = new CancellationTokenSource();
+            var token = cts.Token;
+
+            Task task = new Task(() =>
+            {
+                for (int i=0;i<int.MaxValue;i++)
+                {
+                    if (token.IsCancellationRequested)
+                    {
+                        Console.WriteLine("Task is canceled");
+                        throw new OperationCanceledException(token);
+                    }
+                    else
+                    {
+                        Console.Write(".");
+                    }
+                }
+            },token);
+
+            Console.WriteLine("Press key to start");
+            Console.ReadLine();
+            task.Start();
+            Console.ReadLine();
+            cts.Cancel();
+            Console.WriteLine("Done!");
+            Console.ReadLine();
+
+        }
+    
+    }
+}

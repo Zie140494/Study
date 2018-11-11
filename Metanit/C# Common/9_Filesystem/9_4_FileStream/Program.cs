ac861e2e945756ec97c6152ab7a58257f7273754
ZakharenkoIE <Zie140494@gmail.com>
2018-11-11 15:21:42 +0700

    9.4 запись в файл и чтение из него

--- Metanit/C# Common/9_Filesystem/9_4_FileStream/Program.cs
+++ Metanit/C# Common/9_Filesystem/9_4_FileStream/Program.cs
@@ -16,8 +16,19 @@ namespace _9_4_FileStream
 
             using (FileStream fs = new FileStream(@"C:/Test/Vasa.txt", FileMode.OpenOrCreate))
             {
+                byte[] array = System.Text.Encoding.Default.GetBytes(text);
+                fs.Write(array, 0, text.Length);
+                Console.WriteLine("Данные записаны в файл");
+            }
+            using (FileStream fs = File.OpenRead(@"C:/Test/Vasa.txt"))
+            {
+                byte[] array = new byte[fs.Length];
+                fs.Read(array, 0, array.Length);
+                string textFromFile = System.Text.Encoding.Default.GetString(array);
+                Console.WriteLine($"Текст из файла: {textFromFile}");
 
             }
+            Console.Read();
         }
     }
 }

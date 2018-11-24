5b7dc0f1b52bf337bc3ced439272903229cf1cd7
ZakharenkoIE <Zie140494@gmail.com>
2018-11-24 15:18:15 +0700

    Глава 11. Бинарная сериализация и десериализация массива объектов

---
+++ Metanit/C# Common/11.1_Binary_Serialize/11.1_Binary_Serialize/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _11._1_Binary_Serialize
+{
+    [Serializable]
+    class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public Person(string name, int age)
+        {
+            this.Name = name;
+            this.Age = age;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person("Tom", 27);
+            Person person2 = new Person("Vasya", 30);
+            Person[] people = new Person[] {person,person2};
+            Console.WriteLine("Object is created");
+
+            BinaryFormatter formatter = new BinaryFormatter();
+
+            using (FileStream fs = new FileStream("people.dat", FileMode.OpenOrCreate))
+            {
+                formatter.Serialize(fs, people);
+
+                Console.WriteLine("Объект сериализован");
+            }
+
+            using (FileStream fs = new FileStream("people.dat", FileMode.OpenOrCreate))
+            {
+                Person[] newPerson = (Person[])formatter.Deserialize(fs);
+                Console.WriteLine("Объект десериалзован");
+                foreach (var ns in newPerson)
+                    Console.WriteLine($"Имя:{ns.Name} Возвраст:{ns.Age}");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}

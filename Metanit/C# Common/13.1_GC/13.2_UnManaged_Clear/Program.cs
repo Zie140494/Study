51b13b38e2670ce03d2b805e35736fc777c93233
ZakharenkoIE <Zie140494@gmail.com>
2018-12-15 18:15:19 +0700

    Dispose, указатели

--- Metanit/C# Common/13.1_GC/13.2_UnManaged_Clear/Program.cs
+++ Metanit/C# Common/13.1_GC/13.2_UnManaged_Clear/Program.cs
@@ -15,15 +15,70 @@ namespace _13._2_UnManaged_Clear
             Console.Beep();
         }
     }
+    public class Person:IDisposable
+    {
+        public void Dispose()
+        {
+            Console.Beep();
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
+            Test();
+            Test1();
+            Test2();
+
+            //Указатели
+            unsafe
+            {
+                int* x;
+                int y = 10;
+
+                x = &y;//x=10
+                uint adr = (uint)x;
+                Console.WriteLine($"adress {adr}");
+
+                byte* bytePointer = (byte*)(adr + 4);
+                Console.WriteLine($"byte adr - {*bytePointer}");
+
+                uint oldAdr = (uint)(bytePointer - 4);
+                int* intPointer = (int*)oldAdr;
+                Console.WriteLine($"oldadr - {*intPointer}");
+
+                int** z = &x;
+                Console.WriteLine(**z);
+
+            }
+
             Console.Read();
         }
+        //Destructor
         private static void Test()
         {
             PersonDes person = new PersonDes();
+        }
+        //Dispose1
+        private static void Test1()
+        {
+            Person p = null;
+            try
+            {
+                p = new Person();
+            }
+            finally
+            {
+                if (p != null)
+                    p.Dispose();
+            }
+        }
+        private static void Test2()
+        {
+            using (Person p = new Person())
+            {
+
+            }
         }
     }
 }

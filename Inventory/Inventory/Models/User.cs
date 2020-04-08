2b20963860a13d8584856674ec930aec311dab82
Zakharenko Ilya <zie140494@gmail.com>
2020-04-09 00:13:47 +0700

    Соционика + инвнтаризация

---
+++ Inventory/Inventory/Models/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Inventory.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Adress { get; set; }
+        public string Division { get; set; }
+        public string Status { get; set; }
+        public string Division2 { get; set; }
+        public string Division3 { get; set; }
+        public string Position { get; set; }
+        public string Name { get; set; }
+    }
+}
